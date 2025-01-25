1d84bbf77f9c71187d6e6bf3659b72a04b79b860
Roman Raspik <raspik@gmail.com>
2025-01-25 18:27:55 +0200

    Menu Final

--- Assets/Game/Code/Menu/MenuManager.cs
+++ Assets/Game/Code/Menu/MenuManager.cs
@@ -27,7 +27,10 @@ namespace Menu
         [field: SerializeField] private Button _saveSettingsBtn;
         [field: SerializeField] private Button _closeButton;
         [field: SerializeField] private SoundSettings _soundSettings;
-        
+        [field: SerializeField] private Transform _optionsPlaceholder;
+
+        private float _originalOptionsYPos;
+
         private void OnEnable()
         {
             Vector3 cameraPosition = _bgGameObject.transform.position;
@@ -40,17 +43,25 @@ namespace Menu
             _exitButton.onClick.AddListener(HandleExitClick);
             _saveSettingsBtn.onClick.AddListener(HandleSaveSettingsClick);
             _closeButton.onClick.AddListener(HandleCloseClick);
+            _musicSlider.onValueChanged.AddListener(HandleVolumeChanged);
+        }
+
+        private void HandleVolumeChanged(float arg0)
+        {
+            GetComponent<AudioSource>().volume = arg0;
         }
 
         private void HandleCloseClick()
         {
-            _optionsPanel.transform.DOMoveY(_optionsPanel.transform.position.y*4,1f);
+            _optionsPanel.transform.DOMoveY(_originalOptionsYPos, 2f)
+                .OnComplete(() => _optionsPanel.SetActive(false));
         }
 
         private void HandleSaveSettingsClick()
         {
             _soundSettings.MusicVol = _musicSlider.value;
             _soundSettings.SFXVol =_sfxSlider.value;
+            
             HandleCloseClick();
         }
         
@@ -61,13 +72,23 @@ namespace Menu
 
         private void HandleCreditsClick()
         {
+            if (_optionsPanel.activeSelf)
+            {
+                HandleCloseClick();
+            }
             _creditsPanel.SetActive(true);
         }
 
         private void HandleOptionsClick()
         {
+            if (_optionsPanel.activeSelf)
+            {
+                HandleCloseClick();
+                return;
+            }
             _optionsPanel.SetActive(true);
-            _optionsPanel.transform.DOMoveY(_optionsPanel.transform.position.y/4,1f);
+            _originalOptionsYPos = _optionsPanel.transform.position.y;
+            _optionsPanel.transform.DOMoveY(_optionsPlaceholder.position.y,1f);
         }
 
         private void HandleNewGameClick()
@@ -81,6 +102,9 @@ namespace Menu
             _optionButton.onClick.RemoveListener(HandleOptionsClick);
             _creditsButton.onClick.RemoveListener(HandleCreditsClick);
             _exitButton.onClick.RemoveListener(HandleExitClick);
+            _saveSettingsBtn.onClick.RemoveListener(HandleSaveSettingsClick);
+            _closeButton.onClick.RemoveListener(HandleCloseClick);
+            _musicSlider.onValueChanged.RemoveListener(HandleVolumeChanged);
         }
     }
 }
